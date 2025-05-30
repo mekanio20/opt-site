a447d34cd7b25c947ef1a3efc688a7facb753c4b
mekanio20 <mrxyok138@gmail.com>
2025-05-30 19:57:35 +0500

    privacy

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ const router = createRouter({
       component: () => import("@/views/Home.vue"),
     },
     {
-      path: "/privacy-policy",
+      path: "/privacy",
       name: "PrivacyPolicy",
       component: () => import("@/views/Privacy.vue"),
     },
